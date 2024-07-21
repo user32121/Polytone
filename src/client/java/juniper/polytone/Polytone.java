84cc91c29e5351be4596e4aeaedd0da7a7095e70
jupiter <pengjupiter5@gmail.com>
2024-07-20 18:18:31 -0700

    todo

--- src/client/java/juniper/polytone/Polytone.java
+++ src/client/java/juniper/polytone/Polytone.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import juniper.polytone.init.PolytoneCommand;
 import net.fabricmc.api.ClientModInitializer;
 
-//TODO navigation/pathfinding/flight
 public class Polytone implements ClientModInitializer {
 	public static String MODID = "polytone";
     public static Logger LOGGER = LoggerFactory.getLogger(MODID);
