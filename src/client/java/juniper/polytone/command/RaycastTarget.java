cb04dce30d2ebaba2df1d604de646ddd195dd520
jupiter <pengjupiter5@gmail.com>
2024-07-20 18:43:49 -0700

    move bulk of command code to class

--- src/client/java/juniper/polytone/command/RaycastTarget.java
+++ src/client/java/juniper/polytone/command/RaycastTarget.java
@@ -7,12 +7,16 @@ import java.util.concurrent.CompletableFuture;
 
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.arguments.BoolArgumentType;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.command.CommandSource;
 import net.minecraft.text.Text;
 
@@ -39,4 +43,21 @@ public enum RaycastTarget {
     }
 
     public static final Map<RaycastTarget, Boolean> raycastPriority = new HashMap<>();
+    public static final RequiredArgumentBuilder<FabricClientCommandSource, RaycastTarget> TARGET_ARG = ClientCommandManager.argument("target", new RaycastTargetArgument());
+    public static final RequiredArgumentBuilder<FabricClientCommandSource, Boolean> VALUE_ARG = ClientCommandManager.argument("value", BoolArgumentType.bool());
+
+    public static int getTarget(CommandContext<FabricClientCommandSource> context) {
+        RaycastTarget target = context.getArgument(TARGET_ARG.getName(), RaycastTarget.class);
+        boolean b = RaycastTarget.raycastPriority.getOrDefault(target, false);
+        context.getSource().sendFeedback(Text.literal(String.format("Raycast priority for %s is %s", target, b)));
+        return 1;
+    }
+
+    public static int setTarget(CommandContext<FabricClientCommandSource> context) {
+        RaycastTarget target = context.getArgument(TARGET_ARG.getName(), RaycastTarget.class);
+        boolean b = BoolArgumentType.getBool(context, VALUE_ARG.getName());
+        RaycastTarget.raycastPriority.put(target, b);
+        context.getSource().sendFeedback(Text.literal(String.format("Raycast priority for %s set to %s", target, b)));
+        return 1;
+    }
 }
--- src/client/java/juniper/polytone/init/PolytoneCommand.java
+++ src/client/java/juniper/polytone/init/PolytoneCommand.java
@@ -2,31 +2,18 @@ package juniper.polytone.init;
 
 import java.util.function.Function;
 
-import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 
 import juniper.polytone.Polytone;
 import juniper.polytone.command.RaycastTarget;
-import juniper.polytone.command.RaycastTarget.RaycastTargetArgument;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.text.Text;
 
 public class PolytoneCommand {
     public static void init() {
-        makeCommand("raycast_priority", node -> node.then(ClientCommandManager.argument("target", new RaycastTargetArgument()).executes(context -> {
-            RaycastTarget target = context.getArgument("target", RaycastTarget.class);
-            boolean b = RaycastTarget.raycastPriority.getOrDefault(target, false);
-            context.getSource().sendFeedback(Text.literal(String.format("Raycast priority for %s is %s", target, b)));
-            return 1;
-        }).then(ClientCommandManager.argument("value", BoolArgumentType.bool()).executes(context -> {
-            RaycastTarget target = context.getArgument("target", RaycastTarget.class);
-            boolean b = BoolArgumentType.getBool(context, "value");
-            RaycastTarget.raycastPriority.put(target, b);
-            context.getSource().sendFeedback(Text.literal(String.format("Raycast priority for %s set to %s", target, b)));
-            return 1;
-        }))));
+        makeCommand("raycast_priority",
+                node -> node.then(RaycastTarget.TARGET_ARG.executes(RaycastTarget::getTarget).then(RaycastTarget.VALUE_ARG.executes(RaycastTarget::setTarget))));
     }
 
     private static void makeCommand(String command, Function<LiteralArgumentBuilder<FabricClientCommandSource>, LiteralArgumentBuilder<FabricClientCommandSource>> buildCommand) {
